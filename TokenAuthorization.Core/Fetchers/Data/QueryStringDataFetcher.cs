30f8addea7066a7e9def5b29be4fe017ff3b001f
Patrik <rambo3swe@gmail.com>
2013-12-23 20:01:54 +0100

    added headerdata featcher

--- TokenAuthorization.Core/Fetchers/Data/QueryStringDataFetcher.cs
+++ TokenAuthorization.Core/Fetchers/Data/QueryStringDataFetcher.cs
@@ -2,6 +2,7 @@ using System;
 using System.Linq;
 using System.Net.Http;
 using TokenAuthorization.Core.Caching;
+using TokenAuthorization.Core.Extensions;
 
 namespace TokenAuthorization.Core.Fetchers.Data
 {
@@ -28,6 +29,22 @@ namespace TokenAuthorization.Core.Fetchers.Data
         public override string CacheableFetchData(HttpRequestMessage request)
         {
             return request.GetQueryNameValuePairs().FirstOrDefault(p => p.Key == DataName).Value;
+        }
+    }
+
+    public class HeaderDataFetcher : CacheableDataFetcher
+    {
+        public HeaderDataFetcher(string dataName, IDataCacheStrategy cacheStrategy) : base(dataName, cacheStrategy)
+        {
+        }
+
+        public HeaderDataFetcher(Lazy<string> lazyDataName, IDataCacheStrategy cacheStrategy) : base(lazyDataName, cacheStrategy)
+        {
+        }
+
+        public override string CacheableFetchData(HttpRequestMessage request)
+        {
+            return request.GetHeader(DataName);
         }
     }
 }
---
+++ TokenAuthorization.Core/Fetchers/Token/HeaderTokenFetcher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using TokenAuthorization.Core.Caching;
+using TokenAuthorization.Core.Configuration;
+using TokenAuthorization.Core.Fetchers.Data;
+
+namespace TokenAuthorization.Core.Fetchers.Token
+{
+    public class HeaderTokenFetcher : CookieDataFetcher, ITokenFetcher
+    {
+        public HeaderTokenFetcher()
+            : base(new Lazy<string>(() => TokenAuthenticationConfiguration.TokenName), new TokenCacheStrategy())
+        {
+        }
+    }
+}
